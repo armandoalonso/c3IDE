e141386b39ad58305d6a29b919463f8747606787
armandoalonso1 <armandoalonso1@gmail.com>
2019-01-17 17:13:54 -0500

    add new console app to assist in generating auto completion from scrapped documentation =)

---
+++ AutoCompletionGenerator/AutoCompletionPoco.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace AutoCompletionGenerator
+{
+    public class AutoCompletionPoco
+    {
+        public string Text { get; set; }
+        public int Type { get; set; }
+        public string Container { get; set; }
+        public string DescriptionText { get; set; }
+    }
+}
---
+++ AutoCompletionGenerator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace AutoCompletionGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = System.IO.File.ReadAllLines("input.txt");
+            var count = lines.Length;
+            var curLine = 0;
+
+            var list = new List<AutoCompletionPoco>();
+
+            while (curLine < count)
+            {
+                var text = lines[curLine];
+                var type = lines[curLine + 1];
+                var container = lines[curLine + 2];
+                var desc = lines[curLine + 3];
+
+                var obj = new AutoCompletionPoco
+                {
+                    Text = text,
+                    Type = int.Parse(type),
+                    DescriptionText = desc,
+                    Container = container,
+                };
+
+                list.Add(obj);
+
+                curLine = curLine+4;
+            }
+
+            System.IO.File.WriteAllText("output.json", JsonConvert.SerializeObject(list, Formatting.Indented));
+            Process.Start("output.json");
+        }
+    }
+}
