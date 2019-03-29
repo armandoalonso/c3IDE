56daf7b4dc0f6c89bca6aed7feaef200a5507867
Armando Alonso <armando_alonso@ultimatesoftware.com>
2019-03-29 12:10:05 -0400

    add ctor and logging to application window manager

--- c3IDE/Managers/ApplicationWindows.cs
+++ c3IDE/Managers/ApplicationWindows.cs
@@ -9,22 +9,51 @@ namespace c3IDE.Managers
 {
     public static class ApplicationWindows
     {
-        public static DashboardWindow DashboardWindow = new DashboardWindow();
-        public static AddonMetadataWindow MetadataWindow = new AddonMetadataWindow();
-        public static AddonWindow AddonWindow = new AddonWindow();
-        public static PluginWindow PluginWindow = new PluginWindow();
-        public static TypeWindow TypeWindow = new TypeWindow();
-        public static InstanceWindow InstanceWindow = new InstanceWindow();
-        public static ActionWindow ActionWindow = new ActionWindow();
-        public static ConditionWindow ConditionWindow = new ConditionWindow();
-        public static ExpressionWindow ExpressionWindow = new ExpressionWindow();
-        public static LanguageWindow LanguageWindow = new LanguageWindow();
-        public static TestWindow TestWidnow = new TestWindow();
-        public static OptionsWindow OptionsWindow = new OptionsWindow();
+        public static DashboardWindow DashboardWindow;
+        public static AddonMetadataWindow MetadataWindow;
+        public static AddonWindow AddonWindow;
+        public static PluginWindow PluginWindow;
+        public static TypeWindow TypeWindow;
+        public static InstanceWindow InstanceWindow;
+        public static ActionWindow ActionWindow;
+        public static ConditionWindow ConditionWindow;
+        public static ExpressionWindow ExpressionWindow;
+        public static LanguageWindow LanguageWindow;
+        public static TestWindow TestWidnow;
+        public static OptionsWindow OptionsWindow;
         public static PopoutCompileWindow PopoutCompileWindow;
-        public static SearchAndReplaceWindow FindAndReplaceWindow = new SearchAndReplaceWindow();
-        public static EffectPropertiesWindow EffectPropertiesWindow = new EffectPropertiesWindow();
-        public static EffectParameterWindow EffectParameterWindow = new EffectParameterWindow();
-        public static EffectCodeWindow EffectCodeWindow = new EffectCodeWindow();
+        public static SearchAndReplaceWindow FindAndReplaceWindow;
+        public static EffectPropertiesWindow EffectPropertiesWindow;
+        public static EffectParameterWindow EffectParameterWindow;
+        public static EffectCodeWindow EffectCodeWindow;
+
+        static ApplicationWindows()
+        {
+            try
+            {
+                DashboardWindow = new DashboardWindow();
+                MetadataWindow = new AddonMetadataWindow();
+                AddonWindow = new AddonWindow();
+                PluginWindow = new PluginWindow();
+                TypeWindow = new TypeWindow();
+                InstanceWindow = new InstanceWindow();
+                ActionWindow = new ActionWindow();
+                ConditionWindow = new ConditionWindow();
+                ExpressionWindow = new ExpressionWindow();
+                LanguageWindow = new LanguageWindow();
+                TestWidnow = new TestWindow();
+                OptionsWindow = new OptionsWindow();
+                FindAndReplaceWindow = new SearchAndReplaceWindow();
+                EffectPropertiesWindow = new EffectPropertiesWindow();
+                EffectParameterWindow = new EffectParameterWindow();
+                EffectCodeWindow = new EffectCodeWindow();
+            }
+            catch (Exception ex)
+            {
+                LogManager.AddErrorLog(ex);
+                throw;
+            }
+
+        }
     }
 }
