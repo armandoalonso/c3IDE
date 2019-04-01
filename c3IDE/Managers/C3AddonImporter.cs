cac398fde174cfb9aca73c98b966c2f320a54a9b
armandoalonso1 <armandoalonso1@gmail.com>
2019-04-01 06:57:53 -0400

    add extra comments to c3addon importer test

--- c3IDE/Managers/C3AddonImporter.cs
+++ c3IDE/Managers/C3AddonImporter.cs
@@ -5,6 +5,7 @@ using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using c3IDE.Models;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -17,7 +18,6 @@ namespace c3IDE.Managers
             var fi = new FileInfo(path);
             var tmpPath = OptionsManager.CurrentOptions.DataPath + "\\tmp";
             if(Directory.Exists(tmpPath)) Directory.Delete(tmpPath, true);
-
 
             //unzip c3addon to temp location
             ZipFile.ExtractToDirectory(path, tmpPath);
@@ -43,21 +43,42 @@ namespace c3IDE.Managers
                 var ace = JObject.Parse(File.ReadAllText(Path.Combine(tmpPath, "aces.json")));
                 var lang = JObject.Parse(File.ReadAllText(Path.Combine(tmpPath, "lang", "en-US.json")))["text"][type+"s"][id.ToLower()];
 
+                var actionList = new List<Models.Action>();
+
                 foreach (JProperty category in ace.Properties())
                 {
+                    //parse actions
                     var actions = JArray.Parse(ace[category.Name]["actions"].ToString());
                     foreach (var action in actions.Children<JObject>())
                     {
                         var actionId = action["id"].ToString();
                         var actionAce = action.ToString();
                         var actionLang = $"\"{actionId}\":" + lang["actions"][actionId];
+                        var actionScript = action["scriptName"].ToString();
                         var actionParams = action["params"].Children<JObject>().Select(x => x["id"].ToString());
+
+                        //todo: currently code is being stubbed out, find way to parse action.js
+                        var act = new Models.Action
+                        {
+                            Id = actionId,
+                            Category = category.Name,
+                            Ace = actionAce,
+                            Language = actionLang,
+                            Code = $"{actionScript}({string.Join(",", actionParams)}) {{ \n}}"
+                        };
+
+                        actionList.Add(act);
                     }
 
+                    //parse conditions
                     var conditions = JArray.Parse(ace[category.Name]["conditions"].ToString());
                    
                     var expressions = JArray.Parse(ace[category.Name]["expressions"].ToString());
                 }
+
+                //todo: create c3addon, and map parsed data to c3addon 
+
+                //todo: run export process on created c3addon (or import into c3ide)
             }
             else
             {
