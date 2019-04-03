8c470aa850e79f365e61928aff008c6efa8d4061
Armando Alonso <armando_alonso@ultimatesoftware.com>
2019-04-03 14:31:26 -0400

    create c2 addon import => converts edittime.js to knwon c2 model, used https://github.com/Armaldio/c2-addon-parser implementation of ast traversal ported to DotNet. next step invovle mapping c2 models to c3 models.

---
+++ c3IDE.Tests/JavascriptParsingTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using c3IDE.Managers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace c3IDE.Tests
+{
+    [TestClass]
+    public class JavascriptParsingTest
+    {
+
+        [TestMethod]
+        public void TestActionParseHappyPath()
+        {
+            var text = File.ReadAllText("TestFiles\\stack_edittime.js");
+            C2AddonImporter.Insatnce.ReadEdittimeJs(text);
+        }
+
+    }
+}
+    
