6ac2a0af0401fa7efad4102dedda18f5b5fe76a8
ArmandoA <armando_alonso@ultimatesfotware.com>
2019-10-16 16:35:06 -0400

    Fix build version when compiling addons

--- c3IDE/MainWindow.xaml.cs
+++ c3IDE/MainWindow.xaml.cs
@@ -21,7 +21,7 @@ namespace c3IDE
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
-        private const string Version = "1.1.0.68";
+        private const string Version = "1.1.0.69";
 
         /// <summary>
         /// main window constructor
